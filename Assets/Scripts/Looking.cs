df81cfff082f820456b3d97eeec52ee6635baebe
Arash Izadi <arash.iz@live.com>
2021-03-17 15:21:30 -0400

    added movement to Ekemini's scene

---
+++ Assets/Scripts/Looking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looking : MonoBehaviour
+{
+	//Multipler
+	public float MouseSensitivity = 200f;
+	//Mouse Positions
+	float MouseX;
+	float MouseY;
+
+	//Player Body
+	public Transform Body;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        //Player Body Turn
+        Body.Rotate(Vector3.up * MouseX);
+        //Camera Rotation
+        transform.Rotate(Vector3.right * -MouseY);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	CharacterController PlayerController;
+
+	//Movement
+	Vector3 PlayerMovement;
+    float PlayerX;
+    float PlayerZ;
+    //Multipler
+    public float MovementSpeed = 5;
+
+    //Gravity
+    Vector3 GravityVector;
+    public float Gravity = -9.81f;
+
+    void Start()
+    {
+        PlayerController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+    	//Movement positions
+        PlayerX = Input.GetAxis("Horizontal");
+        PlayerZ = Input.GetAxis("Vertical");
+        PlayerMovement = transform.right * PlayerX + transform.forward * PlayerZ;
+
+        //Gravity Position
+        GravityVector = new Vector3(0, Gravity, 0);
+
+        //To See Code Works
+        Debugging();
+
+        //Movement
+        PlayerController.Move(PlayerMovement * MovementSpeed * Time.deltaTime);
+        //Gravity
+        PlayerController.Move(GravityVector * Time.deltaTime);
+    }
+
+	//Debugs
+	void Debugging()
+	{
+		//Touching Ground
+        /*if(PlayerController.isGrounded)
+        {
+        	Debug.Log("Grounded");
+        }*/
+
+        //Console Writes
+    	if(Input.GetKey(KeyCode.W))
+    	{
+    		Debug.Log("Forward");
+    	}
+    	else if(Input.GetKey(KeyCode.S))
+    	{
+    		Debug.Log("Backwards");
+    	}
+    	else if(Input.GetKey(KeyCode.D))
+    	{
+    		Debug.Log("Right");
+    	}
+        else if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left");
+        }
+	}
+}
