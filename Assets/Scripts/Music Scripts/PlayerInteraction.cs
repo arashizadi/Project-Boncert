5fc4deddef91399ff001bfa865f5512c371cd5c4
Bryan Santiago <musicprostar11@gmail.com>
2021-04-07 18:09:07 -0400

    Re-did the song changer

---
+++ Assets/Scripts/Music Scripts/PlayerInteraction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
