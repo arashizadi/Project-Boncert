1015387835389cd9c9c3bcf582d4f6ed2b98d888
Ekemini Nkanta <3k3m1n1@gmail.com>
2021-04-14 14:04:11 -0400

    restored Bryan's song changer
    please work

---
+++ Assets/Scripts/Music Scripts/SongTriggerScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongTriggerScript : MonoBehaviour
+{
+	public GameObject SongUI;
+
+    void Start()
+    {
+        SongUI.SetActive(false);
+    }
+
+    /*void Update()
+    {
+
+    }*/
+
+    void OnTriggerEnter(Collider SongMenuEnter)
+    {
+    	if(SongMenuEnter.gameObject.tag == "Player")
+    	{
+    		SongUI.SetActive(true);
+            //Debug.Log("UI Active");
+    	}
+    }
+
+    void OnTriggerExit(Collider SongMenuExit)
+    {
+        if(SongMenuExit.gameObject.tag == "Player")
+        {
+    	   SongUI.SetActive(false);
+           //Debug.Log("UI Deactive");
+        }
+    }
+}
