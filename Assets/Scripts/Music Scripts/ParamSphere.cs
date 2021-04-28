bab9bcc19f3a4247a89960c216901f223349e143
Bryan Santiago <musicprostar11@gmail.com>
2021-04-28 17:56:51 -0400

    Audio Equalizer Inside City

--- Assets/Scripts/Music Scripts/ParamSphere.cs
+++ Assets/Scripts/Music Scripts/ParamSphere.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParamCube : MonoBehaviour
+public class ParamSphere : MonoBehaviour
 {
     [HideInInspector] public int _band;
     [SerializeField] private float _startScale = 1f;
@@ -24,7 +24,7 @@ public class ParamCube : MonoBehaviour
         float resizeAmount = FMODAudioVisualizer.bandBuffer[_band] * _scaleMultipler;
 
         transform.position = new Vector3(transform.position.x, startPos, transform.position.z);
-        transform.localScale = new Vector3(_startScale, resizeAmount + _startScale, _startScale);
-        transform.position = new Vector3(transform.position.x, transform.position.y + resizeAmount / 2, transform.position.z);
+        transform.localScale = new Vector3(resizeAmount + _startScale, resizeAmount + _startScale, resizeAmount + _startScale);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
     }
 }
