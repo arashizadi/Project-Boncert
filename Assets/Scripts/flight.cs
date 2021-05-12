294a1d3fb605ad9fbefdaf8d2e56596eef69d79e
Ekemini Nkanta <3k3m1n1@gmail.com>
2021-05-12 00:12:06 -0400

    slowed down vertical movement for drone
    lol i forgot Time.deltaTime
    btw the transform.Translate line i switched to ignores colliders :( but it's the only method that wouldn't stutter so gg

--- Assets/Scripts/flight.cs
+++ Assets/Scripts/flight.cs
@@ -22,12 +22,12 @@ public class flight : NetworkBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             //drone.position = drone.position + new Vector3(0f, 1f, 0f) * speed * Time.deltaTime;
-            transform.Translate(0f, 1f, 0f);
+            transform.Translate(0f, speed * Time.deltaTime, 0f);
         }
         else if (Input.GetKey(KeyCode.LeftShift))
         {
             //drone.position = drone.position + new Vector3(0f, -1f, 0f) * speed * Time.deltaTime;
-            transform.Translate(0f, -1f, 0f);
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
         }
 
 
